4c87553254fce8fd5e8ab7e368862800bd50559d
judomo <igorzhukovsky123@gmail.com>
2020-06-16 20:28:40 +0300

    Fixed login and usercheck

---
+++ build/precache-manifest.17575d2d0da48ca423747093666f045f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7de0bd69c7e1f9d2f78968ace6e80e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb790ae72ee4ead97a77",
+    "url": "/static/css/2.0a896841.chunk.css"
+  },
+  {
+    "revision": "de7cbe046f430916a095",
+    "url": "/static/css/main.51fde8d1.chunk.css"
+  },
+  {
+    "revision": "eb790ae72ee4ead97a77",
+    "url": "/static/js/2.302d68dd.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.302d68dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7cbe046f430916a095",
+    "url": "/static/js/main.b1e0722f.chunk.js"
+  },
+  {
+    "revision": "584b9e79d191c9e1b9a9",
+    "url": "/static/js/runtime-main.a0243d46.js"
+  },
+  {
+    "revision": "f0a560fa6bc6acbb31987e4523dad71e",
+    "url": "/static/media/camp.f0a560fa.jpg"
+  }
+]);
